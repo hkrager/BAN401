c738c0b72f01efb8cf0910e1f5adf38b04fdad29
Yngve <yngve.sture@gmail.com>
2019-11-05 18:46:19 +0100

    First solution on task 5

--- problem_5.R
+++ problem_5.R
@@ -2,3 +2,18 @@
 setwd('C:/Users/Yngve/Google Drive/Skolerelatert/NHH/Master/BAN401/BAN401 - Final Assignment')
 
 #### Problem 5
+tab3 <- read.csv('ban401_fuzzy-matching-table3.csv', sep = ';')
+tab4 <- read.csv('ban401_fuzzy-matching-table4.csv', sep = ';')
+
+
+tab5 <- tab3
+applications <- NA*10
+tab5 <- cbind(tab5, applications)
+for (i in 1:length(tab3$Company.name)){
+applications[i] <- length(agrep(droplevels(tab3$Company.name)[i],
+                                droplevels(tab4$Applicant[tab4$Status=="Granted"]), 
+                                ignore.case = T)) # number of applications
+}
+tab5 <- cbind(tab3, applications)
+colnames(tab5)[5] <- "Total number of patents"
+head(tab5, n = 5)
