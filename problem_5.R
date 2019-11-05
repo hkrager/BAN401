1b790f1400e57b8615381e604223bcc800ba4409
Yngve <yngve.sture@gmail.com>
2019-11-05 18:52:39 +0100

    Cleaned up code

--- problem_5.R
+++ problem_5.R
@@ -5,15 +5,14 @@ setwd('C:/Users/Yngve/Google Drive/Skolerelatert/NHH/Master/BAN401/BAN401 - Fina
 tab3 <- read.csv('ban401_fuzzy-matching-table3.csv', sep = ';')
 tab4 <- read.csv('ban401_fuzzy-matching-table4.csv', sep = ';')
 
-
 tab5 <- tab3
-applications <- NA*10
-tab5 <- cbind(tab5, applications)
+patents <- NA*10
+tab5 <- cbind(tab5, patents)
 for (i in 1:length(tab3$Company.name)){
-applications[i] <- length(agrep(droplevels(tab3$Company.name)[i],
+patents[i] <- length(agrep(droplevels(tab3$Company.name)[i],
                                 droplevels(tab4$Applicant[tab4$Status=="Granted"]), 
-                                ignore.case = T)) # number of applications
+                                ignore.case = T)) # number of patents
 }
-tab5 <- cbind(tab3, applications)
+tab5 <- cbind(tab3, patents)
 colnames(tab5)[5] <- "Total number of patents"
 head(tab5, n = 5)
