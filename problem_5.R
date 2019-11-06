9fcd7aad727e86346f7019439701210ce597c621
Yngve <yngve.sture@gmail.com>
2019-11-06 17:34:44 +0100

    Updated code aestetics

--- problem_5.R
+++ problem_5.R
@@ -8,9 +8,9 @@ tab4 <- read.csv('ban401_fuzzy-matching-table4.csv', sep = ';')
 tab5 <- tab3 # table 5 is going to include the whole content of table 3, plus one new column
 patents <- c() # initializing new column where amount of patents will be kept
 for (i in 1:length(tab3$Company.name)){ # calculating number of granted patents for each company
-patents[i] <- length(agrep(droplevels(tab3$Company.name)[i],
-                           droplevels(tab4$Applicant[tab4$Status=="Granted"]), 
-                           ignore.case = T)) 
+  patents[i] <- length(agrep(droplevels(tab3$Company.name)[i],
+                             droplevels(tab4$Applicant[tab4$Status=="Granted"]), 
+                             ignore.case = T)) 
 }
 tab5 <- cbind(tab3, patents) # combining the new patents column with the exisiting table 5
 colnames(tab5)[5] <- "Total number of patents" # changing column
