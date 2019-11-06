6e991fab8e93b08ce742ce2d9ae914774efb715b
Yngve <yngve.sture@gmail.com>
2019-11-06 09:02:38 +0100

    Annotated code and reduced lines of code

--- problem_5.R
+++ problem_5.R
@@ -5,14 +5,14 @@ setwd('C:/Users/Yngve/Google Drive/Skolerelatert/NHH/Master/BAN401/BAN401 - Fina
 tab3 <- read.csv('ban401_fuzzy-matching-table3.csv', sep = ';')
 tab4 <- read.csv('ban401_fuzzy-matching-table4.csv', sep = ';')
 
-tab5 <- tab3
-patents <- NA*10
-tab5 <- cbind(tab5, patents)
-for (i in 1:length(tab3$Company.name)){
+tab5 <- tab3 # table 5 is going to include the whole content of table 3, plus one new column
+patents <- c() # initializing new column where amount of patents will be kept
+for (i in 1:length(tab3$Company.name)){ # calculating number of granted patents for each company
 patents[i] <- length(agrep(droplevels(tab3$Company.name)[i],
-                                droplevels(tab4$Applicant[tab4$Status=="Granted"]), 
-                                ignore.case = T)) # number of patents
+                           droplevels(tab4$Applicant[tab4$Status=="Granted"]), 
+                           ignore.case = T)) 
 }
-tab5 <- cbind(tab3, patents)
-colnames(tab5)[5] <- "Total number of patents"
-head(tab5, n = 5)
+tab5 <- cbind(tab3, patents) # combining the new patents column with the exisiting table 5
+colnames(tab5)[5] <- "Total number of patents" # changing column
+tab5 <- tab5[order(tab5$`Total number of patents`, decreasing = T),] # ordering data frame after number of granted patents
+head(tab5, n = 5) # displaying five companies with highest amount of granted patents
