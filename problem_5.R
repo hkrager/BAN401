22680face5648943ee98dd4185a14d6c39404367
Yngve <yngve.sture@gmail.com>
2019-11-07 15:27:45 +0100

    Reduced lines, new solution

--- problem_5.R
+++ problem_5.R
@@ -6,23 +6,20 @@ tab3 <- read.csv('ban401_fuzzy-matching-table3.csv', sep = ';')
 tab4 <- read.csv('ban401_fuzzy-matching-table4.csv', sep = ';')
 
 tab5 <- tab3 # table 5 is going to include the whole content of table 3, plus one new column
-grantedPatents <- list() # initializing new column where amount of patents will be kept
-companies <- list()
+grantedPatents <- list(); companies <- list() # initializing new column where amount of patents will be kept
+
 
 for(i in 1:length(tab3$Company.name)){
   companies[i] <- (strsplit(gsub("(?<=[a-z])(?=[A-Z])", perl = TRUE, ';\\1', 
-                                 as.character(droplevels(tab3$Company.name)[i])), 
-                            ";"))
+                                 as.character(droplevels(tab3$Company.name)[i])), ";"))
   
   for (j in length(companies[[i]])){
-    numOfGranted <- (length(agrep(companies[[i]][j], 
-                                  droplevels(tab4$Applicant[tab4$Status=="Granted"]))))
+    amountGranted <- length(agrep(companies[[i]][j], ignore.case = T),
+                                   droplevels(tab4$Applicant[tab4$Status=="Granted"]))
     
-    if (numOfGranted != 0){grantedPatents[i] <- numOfGranted} 
-    else{grantedPatents[i] <- 0}
+    if (amountGranted != 0){grantedPatents[i] <- amountGranted} else{grantedPatents[i] <- 0}
+    }
   }
-  }
-
 tab5$`Total number of patents` <- unlist(grantedPatents)
 tab5 <- tab5[order(tab5$`Total number of patents`, decreasing = T),] # ordering data frame after number of granted patents
 head(tab5, n = 5) # displaying five companies with highest amount of granted patents
